b3308748fa80151f77d9de81d5491be3dded818f
Thomas Blazek <wtblazek@gmail.com>
2017-11-13 13:54:40 +0100

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,8 +59,11 @@
 
 
 
+                    <h2>Email Kontakt</h2>
+                    {{Html::mailto('info@padddysreturn.com')}}
                     <h2>Facebook</h2>
-                    <div class="fb-page" data-href="https://www.facebook.com/paddysreturnvienna/" data-tabs="timeline" data-width="400" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/paddysreturnvienna/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/paddysreturnvienna/">Paddy&#039;s Return</a></blockquote></div>
+                    <div class="fb-page" data-href="https://www.facebook.com/paddysreturnvienna/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/paddysreturnvienna/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/paddysreturnvienna/">Paddy&#039;s Return</a></blockquote></div>
+                    
                 </div>
 
             </div>
