c90422c2e4c0b3af655b2b7453c58f146e24ba63
Thomas Blazek <wtblazek@gmail.com>
2018-01-04 16:07:54 +0100

    updates

---
+++ app/Mail/DataUpdate.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class DataUpdate extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $removals;
+    public $additions;
+    public $order_changes;
+    public $title;
+    public $url;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($removals, $additions, $order_changes, $title, $url)
+    {
+        //
+        $this->removals =$removals;
+        $this->additions =$additions;
+        $this->order_changes =$order_changes;
+        $this->title =$title;
+        $this->url = $url;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from("mailer@paddysreturn.com")->view('mailers.data');
+    }
+}
