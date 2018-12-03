a94fa7a31aa34f5c862f42c1ed7bfec636fd85c7
Thomas Blazek <wtblazek@gmail.com>
2018-12-03 11:01:32 +0100

    shadow

--- resources/views/announcements/title.blade.php
+++ resources/views/announcements/title.blade.php
@@ -6,9 +6,9 @@
         </div>
         <li class="lazy-bg" data-background=" {{$a->photo_file_name }} ">
             <div class="container">
-                <h2 class="slide-title" stlye="text-shadow:2px 2px 2px #000000">  {{$a->title}}</h2>
-                <h3 class="slide-subtitle" stlye="text-shadow:2px 2px 2px #000000"> {{$a->message}} </h3>
-                <p class="slide-desc hidden-xs hidden-sm "stlye="text-shadow:2px 2px 2px #000000""> {{$a->text}}</p>
+                <h2 class="slide-title" style="text-shadow:2px 2px 2px #000000">  {{$a->title}}</h2>
+                <h3 class="slide-subtitle" style="text-shadow:2px 2px 2px #000000"> {{$a->message}} </h3>
+                <p class="slide-desc hidden-xs hidden-sm " style="text-shadow:2px 2px 2px #000000""> {{$a->text}}</p>
                
                 <a href="{{$a->link}}" class="button cut-corner">Mehr Lesen</a>
             </div>
