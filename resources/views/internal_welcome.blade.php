effd75ba2c0e8ffff2526edfdea2ee5a6554d2c3
Thomas Blazek <wtblazek@gmail.com>
2019-01-07 13:57:19 +0100

    fix

--- resources/views/internal_welcome.blade.php
+++ resources/views/internal_welcome.blade.php
@@ -14,7 +14,7 @@ Paddy’s Return - Pure Irish Folk Music in Wien!
       <div class="col-sm-6">
         <h2> Zukünftige Konzerte </h2>
         @foreach($konzerte as $k)
-        <div>$k->start_at: <a href="/konzerte/{{$k->id}}">{{$k->title}}</a>: {!! null ===$k->setlist ? '<a href="/internal/setlists/create?konzerte='.$k->id.'">Keine Setlist Vorhanden, neue erstellen</a>': '<a href="/internal/setlists/'.$k->setlist->id.'>'.$k->setlist->title."</a>" !!}</div>
+        <div>{{$k->start_at}}: <a href="/konzerte/{{$k->id}}">{{$k->title}}</a>: {!! null ===$k->setlist ? '<a href="/internal/setlists/create?konzerte='.$k->id.'">Keine Setlist Vorhanden, neue erstellen</a>': '<a href="/internal/setlists/'.$k->setlist->id.'>'.$k->setlist->title."</a>" !!}</div>
         @endforeach
       </div>
 <div class="col-sm-6">
