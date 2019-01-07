c823c985c0124db486ede907e96cc66412e7a75f
Thomas Blazek <wtblazek@gmail.com>
2019-01-07 13:24:03 +0100

    fix

--- resources/views/internal_welcome.blade.php
+++ resources/views/internal_welcome.blade.php
@@ -1,14 +1,27 @@
 @extends('layouts/internal')
 @section('PageTitle', 'Übersicht')
+@section('description')
+Paddy’s Return - Pure Irish Folk Music in Wien!
+@endsection
+@section('content')
 <div class="page-title">
-    <h1 class="text-center">Interne Übersicht</h1>
+  <h1 class="text-center">Interne Übersicht</h1>
 </div>
 <main class="main">
 
-    <div class="fullwidth-block">
-        <div class="container">
-            <div class="col-sm-3"></div>
-        </div>
+  <div class="fullwidth-block">
+    <div class="container">
+      <div class="col-sm-6">
+        @foreach($konzerte as $k)
+         <div>{{$k->title}}: {{is_null($k->setlist()) ? "Keine Setlist Vorhanden" : $k->setlist()->title }}
+        @endforeach
+      </div>
+      <div class="col-sm-6">
+        @foreach($tunes as $t)
+        <div>{{$t->name}}: {{$t->updated_at}}</div>
+        @endforeach
+      </div>
     </div>
+  </div>
 </main>
 @endsection
